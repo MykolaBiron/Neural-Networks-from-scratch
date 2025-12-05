9c2406ff652670d160e2945b9cb86693213eb2ad
mykola <mykolabiron@gmail.com>
2025-12-05 22:17:28 +0100

    add knn algorithm

--- utils/BaseLayer.py
+++ utils/BaseLayer.py
@@ -1,12 +1,35 @@
-import numpy as np
-
-class BaseLayer:
-    def __init__(self):
-        self.input = None
-        self.output = None
-
-    def forward(self, input):
-        pass
-    
-    def backward(self, output_gradient, learning_rate):
-        pass
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "c0acd2f9",
+   "metadata": {},
+   "source": [
+    "## Implement core functions and classes used to build a neural network"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d399045b",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import numpy as np"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "env",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "name": "python",
+   "version": "3.12.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
